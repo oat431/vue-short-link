9fe10da66000639827336a4ac10d09ed648e1cf5
oat431 <oat431@gmail.com>
2026-01-13 22:29:01 +0700

    setup shortlink frontend

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="lg:mx-60 lg:my-6 lg:px-3 sm:mx-10 sm:my-2 sm:px-1">
+    <div class="overflow-x-auto">
+      <div class="text-center mt-6">
+        <h1 class="text-2xl font-bold">Short Link</h1>
+      </div>
+    </div>
+  </div>
+</template>
